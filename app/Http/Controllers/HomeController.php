6f94ddd2602ba764390ed9698d0687199849a8dd
ArneVanTulden <arne.vantulden@student.kdg.be>
2026-06-08 01:27:55 +0200

    hotfix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,7 @@ class HomeController extends Controller
     public function index()
     {
         $recepten = Recipe::orderBy('id')->limit(3)->get();
-        $sushiRecept = Recipe::where('title', 'Sushi Bowl met Balls & Glory Challenge')->first();
+        $sushiRecept = Recipe::find(6);
         return view('home', compact('recepten', 'sushiRecept'));
     }
 }
