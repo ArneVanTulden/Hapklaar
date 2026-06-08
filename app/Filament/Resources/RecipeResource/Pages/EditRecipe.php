9b2d3cc6f091aaa6528f92a8c86c34cef102613a
ArneVanTulden <arne.vantulden@student.kdg.be>
2026-06-08 19:14:16 +0200

    voedingswaarde herekenen knop

--- app/Filament/Resources/RecipeResource/Pages/EditRecipe.php
+++ app/Filament/Resources/RecipeResource/Pages/EditRecipe.php
@@ -3,7 +3,10 @@
 namespace App\Filament\Resources\RecipeResource\Pages;
 
 use App\Filament\Resources\RecipeResource;
+use App\Models\NutritionInfo;
+use App\Services\USDANutritionService;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditRecipe extends EditRecord
@@ -13,7 +16,32 @@ class EditRecipe extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
+            Actions\Action::make('recalculate_nutrition')
+                ->label('Herbereken voedingswaarden')
+                ->icon('heroicon-o-arrow-path')
+                ->color('warning')
+                ->action(function () {
+                    $this->recalculateNutrition();
+                    Notification::make()
+                        ->title('Voedingswaarden herberekend')
+                        ->success()
+                        ->send();
+                }),
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function afterSave(): void
+    {
+        $this->recalculateNutrition();
+    }
+
+    private function recalculateNutrition(): void
+    {
+        $recipe = $this->getRecord()->load('ingredients');
+        $totals = app(USDANutritionService::class)->calculateForRecipe($recipe);
+
+        NutritionInfo::updateOrCreate(['recipe_id' => $recipe->id], $totals);
+        $recipe->update(['calories_per_portion' => round($totals['calories'])]);
     }
 }
